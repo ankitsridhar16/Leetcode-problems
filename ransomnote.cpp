a9502e4c3902c84291348daee45a40f1703facb5
ankitsridhar16 <ankit.169108023@muj.manipal.edu>
2020-05-03 18:47:33 +0530

    day3

---
+++ ransomnote.cpp
@@ -0,0 +1,24 @@
+/**
+ * Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+
+Each letter in the magazine string can only be used once in your ransom note.
+**/
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        if(ransomNote.size()>magazine.size())
+            return false;
+        if(ransomNote.size()==0)
+            return true;
+        unordered_map<int,int> pool ;
+        for(int i=0; i<magazine.size(); i++)
+            pool[magazine[i]]++;
+        for(int i=0;i<ransomNote.size();i++){
+            pool[ransomNote[i]]--;
+            if(pool[ransomNote[i]]<0)
+                return false;
+        }
+        return true;
+    }
+};
