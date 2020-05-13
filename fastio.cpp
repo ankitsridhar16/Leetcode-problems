1c526024ebfab520f79a1595d7fdfd9ef35bf6a6
ankitsridhar16 <ankit.169108023@muj.manipal.edu>
2020-05-14 05:24:39 +0530

    fast io

---
+++ fastio.cpp
@@ -0,0 +1,8 @@
+//add this to increase runtime [hack]
+
+static auto fastio = [](){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
