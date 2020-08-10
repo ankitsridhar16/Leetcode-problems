e56ac667472a16a69f738d46a6dc7ddde99362d0
ankitsridhar16 <ankit.169108023@muj.manipal.edu>
2020-08-11 00:01:02 +0530

    added more array problems

---
+++ array/square_sorted.cpp
@@ -0,0 +1,23 @@
+// Squares of a Sorted Array
+
+
+// Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
+
+ 
+
+// Example 1:
+
+// Input: [-4,-1,0,3,10]
+// Output: [0,1,9,16,100]
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        for(int i=0;i<A.size();++i)
+        {
+            A[i]= A[i]*A[i];
+        }
+        sort(A.begin(), A.end());
+        return A;
+    }
+};
