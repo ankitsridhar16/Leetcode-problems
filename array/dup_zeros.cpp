5235b7847a4b4c85e314cb72363ddcca953c5003
ankitsridhar16 <ankit.169108023@muj.manipal.edu>
2020-08-11 23:41:36 +0530

    added dup zero

---
+++ array/dup_zeros.cpp
@@ -0,0 +1,38 @@
+
+
+// Duplicate Zeros
+
+// Given a fixed length array arr of integers, duplicate each occurrence of zero, shifting the remaining elements to the right.
+
+// Note that elements beyond the length of the original array are not written.
+
+// Do the above modifications to the input array in place, do not return anything from your function.
+
+ 
+
+// Example 1:
+
+// Input: [1,0,2,3,0,4,5,0]
+// Output: null
+// Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+
+
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        vector<int> temp;
+        for(int i=0; i<arr.size();++i)
+        {
+            if(!arr[i]==0)
+                temp.push_back(arr[i]);
+            else
+            {
+                temp.push_back(arr[i]);
+                temp.push_back(0);
+            }
+        }
+        int size = arr.size();
+        for(int i=0;i<size;++i)
+            arr[i]=temp[i];
+    }
+};
